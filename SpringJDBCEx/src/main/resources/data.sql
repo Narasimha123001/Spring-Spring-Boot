679c9f8964c38b9a03f44d8445b646783540b59a
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-05-20 23:43:37 +0530

    resources file

---
+++ SpringJDBCEx/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student (rollno, name, marks) values (101, 'hari' , 98);
+insert into student (rollno, name, marks) values (102, 'bhavani', 99);
+insert into student (rollno, name, marks) values (103, 'lakshmi' , 98);
+insert into student (rollno, name, marks) values (104, 'sri' , 100);
