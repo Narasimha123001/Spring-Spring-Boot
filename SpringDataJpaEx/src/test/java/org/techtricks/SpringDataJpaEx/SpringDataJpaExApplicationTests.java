984a1a1fe3fde1510fff34581d95caa4bf37d06a
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-05-21 23:50:30 +0530

    testing file for project

---
+++ SpringDataJpaEx/src/test/java/org/techtricks/SpringDataJpaEx/SpringDataJpaExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.techtricks.SpringDataJpaEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
